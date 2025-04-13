a9348206d7cebc51821d33d10fd92f49c46fe391
lucastgama <lucas_tgama@hotmail.com>
2025-04-13 19:45:04 -0300

    first commit

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,30 @@
+package com.eventostec.api.domain.address;
+
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+
+
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
